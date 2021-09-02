08a42280e7dfac8b8578e5c7adbae95ff628e05d
Shusmoy <1305108.sc@ugrad.cse.buet.ac.bd>
2021-09-02 11:40:31 +0600

    routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,15 +3,15 @@ import express, { Request, Response } from 'express'
 const router = express.Router()
 
 router.get('/api/', async (req: Request, res: Response) => {
-  return res.status(200).send({"msg":"Hello world from get"});
+  return res.status(200).send({ "msg": "Hello world from get" });
 })
 
 router.post('/api/', async (req: Request, res: Response) => {
-  const { title, description } = req.body;
-console.log(title);
-console.log(description);
-  
-  return res.status(201).send({"msg":"Hello world from post"})
+  const { email, password } = req.body;
+  console.log(email);
+  console.log(password);
+
+  return res.status(201).send({ "msg": "Hello world from post" })
 })
 
 export { router as apiRouter }
